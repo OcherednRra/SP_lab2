9a1cbe503bd677dd2981d63fcb90fc504615a60c
flopa_cat <flopa_cat@Ubuntu1488.myguest.virtualbox.org>
2023-12-12 15:40:09 +0200

    add some comment text

--- calculator.cpp
+++ calculator.cpp
@@ -10,6 +10,17 @@ int Calculator::Sub (double a, double b)
     return Add (a, -b);
 }
 
+/*
+Як умру, то поховайте
+Мене на могилі,
+Серед степу широкого,
+На Вкраїні милій,
+Щоб лани широкополі,
+І Дніпро, і кручі
+Було видно, було чути,
+Як реве ревучий.
+*/
+
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
